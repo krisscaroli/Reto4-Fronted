5bbecd9ce77badb7cbfbe086e7ee4551764bf792
krisscaroli <kriss_11_2006@hotmail.com>
2016-02-14 13:13:47 -0500

    reto
    reto4

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+          $(document).ready(function(){
+            $('.modal-trigger').leanModal();
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+
+    $('.button-collapse').sideNav();
+  });
+      
